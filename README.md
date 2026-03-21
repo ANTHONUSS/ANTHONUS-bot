d0c033cb1ba3463e3f337563a04b8bcb74dab6b2
ANTHONUSS <anthonus.d@gmail.com>
2026-03-21 09:28:47 +0100

    init des libs, ajout readme et lisence

---
+++ README.md
@@ -0,0 +1,36 @@
+# ANTHONUS-bot
+
+Bot Discord personnel développé pour l'apprentissage de Kotlin, Java et [JDA](https://jda.wiki), ainsi que pour automatiser quelques tâches du quotidien.
+
+## Objectif du projet
+
+Ce dépôt sert de terrain d'expérimentation autour de :
+- Kotlin sur la JVM
+- L'API Discord via JDA
+- L'intégration de bibliothèques tierces (audio, HTTP, configuration, logs)
+
+## Prérequis
+
+- Java 25 ou supérieur
+
+## Installation et démarrage
+
+1. Télécharger [la dernière release](https://github.com/ANTHONUSS/ANTHONUS-bot/releases/latest).
+2. Lancer le JAR :
+
+```bash
+java -jar ANTHONUS-bot-<version>.jar
+```
+
+## Développement
+
+Compilation locale du projet :
+
+```bash
+./gradlew build
+```
+
+## Licence
+
+Ce projet est distribué sous licence **GNU GPL v3.0**.  
+Voir le fichier [`LICENSE`](LICENSE) pour le texte complet.
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,20 +3,33 @@ plugins {
 }
 
 group = "fr.anthonus"
-version = "1.0-SNAPSHOT"
+version = file("src/main/resources/version.txt").readText().trim()
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    // Source: https://mvnrepository.com/artifact/net.dv8tion/JDA
+    implementation("net.dv8tion:JDA:6.3.2")
+    // Source: https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.5.32")
+
+    // Source: https://mvnrepository.com/artifact/io.github.cdimascio/dotenv-java
+    implementation("io.github.cdimascio:dotenv-java:3.2.0")
+
+    // Source: https://mvnrepository.com/artifact/dev.arbjerg/lavaplayer
+    implementation("dev.arbjerg:lavaplayer:2.2.6")
+    // Source: https://mvnrepository.com/artifact/dev.lavalink.youtube/v2
+    implementation("dev.lavalink.youtube:v2:1.18.0")
+
+    // Source: https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    // Source: https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.13.2")
+
 }
 
 kotlin {
     jvmToolchain(25)
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
